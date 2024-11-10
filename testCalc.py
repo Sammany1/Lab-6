aecab059268227167264d54dac03d19332043b7f
AbdelRahman AlSammany <139874414+Sammany1@users.noreply.github.com>
2024-11-10 05:56:44 -0800

    Update testCalc.py

--- testCalc.py
+++ testCalc.py
@@ -1,14 +1,14 @@
 import unittest
-from calculator import Calculator
+from calculator import Calc
 
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
-        result = Calculator.add(3, 7)
+        result = Calc.add(3, 7)
         self.assertEqual(result, 10)
         
-        result = Calculator.add(-1, 1)
+        result = Calc.add(-1, 1)
         self.assertEqual(result, 0)
         
-        result = Calculator.add(-1, -1)
+        result = Calc.add(-1, -1)
         self.assertEqual(result, -2)
